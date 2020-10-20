d40b1dfb07e4ccbab2a6b78d369a121c6316697f
pencuyn <43643083+pencuyn@users.noreply.github.com>
2020-10-20 11:25:44 -0300

    Add files via upload

---
+++ view/pages/Inglês/Convidados Especiais/ingles - all.php
@@ -0,0 +1,151 @@
+<?php
+
+include "../templates/menu.html";
+
+?>
+<main class="conteudoTelainicial">
+        <div class="titulo">
+            <h1 style="color: black;">Special Guests</h1>
+        </div>
+        <div class="pesquisa">
+            <div id="campos">
+                <form action="">
+                    <div class="pesquisaform">
+                        <label for="Name">Name:</label>
+                        <input type="text" id="Name" name="Name" placeholder="type the name ">
+                        <label for="NacionalAge">NacionalAge:</label>
+                        <input type="text" id="NacionalAge" name="nacionalAge" placeholder="type the country">
+                    </div>
+                    <div class="concluiform">
+                        <a href="" class="btn-concluir">Search</a>  
+                    </div>
+                </form>
+            </div>
+            <div>
+                <a id="pesquisar" class="red-btn">Search</a>
+            </div>
+            </div>
+        </div>
+        <div class="convidadosEspeciais">
+            <!-- começa os exemplos -->
+            <div style="background-image: url(../../css/img/pessoa/andressa.png);">
+                <div class="desc">
+                    <h3>Andressa</h3>
+                 
+                        <p><b>Name:</b> Aishra Valentini</p>
+                        <p><b>Age:</b> 21 anos</p>
+                        <p><b>Email:</b> Aishra@hotmail.com</p>
+                        <a href="../convidadosespeciais/especifico.php"> View</a>
+
+                </div>
+            </div>
+            <div>
+                <div class="desc">
+                    <h3>Andressa</h3>
+                 
+                        <p><b>Name:</b> Aishra Valentini</p>
+                        <p><b>Age:</b> 21 anos</p>
+                        <p><b>Email:</b> Aishra@hotmail.com</p>
+                        <a href=""> View</a>
+
+                </div>
+            </div>
+            <div>
+                <div class="desc">
+                    <h3>Andressa</h3>
+                 
+                        <p><b>Name:</b> Aishra Valentini</p>
+                        <p><b>Age:</b> 21 anos</p>
+                        <p><b>Email:</b> Aishra@hotmail.com</p>
+                        <a href=""> View</a>
+
+                </div>
+            </div>
+            <div style="background-image: url(../../css/img/pessoa/andressa.png);">
+                <div class="desc">
+                    <h3>Andressa</h3>
+                 
+                        <p><b>Name:</b> Aishra Valentini</p>
+                        <p><b>Age:</b> 21 anos</p>
+                        <p><b>Email:</b> Aishra@hotmail.com</p>
+                        <a href=""> View</a>
+
+                </div>
+            </div>
+            <div>
+                <div class="desc">
+                    <h3>Andressa</h3>
+                 
+                        <p><b>Name:</b> Aishra Valentini</p>
+                        <p><b>Age:</b> 21 anos</p>
+                        <p><b>Email:</b> Aishra@hotmail.com</p>
+                        <a href=""> View</a>
+
+                </div>
+            </div>
+            <div>
+                <div class="desc">
+                    <h3>Andressa</h3>
+                 
+                        <p><b>Name:</b> Aishra Valentini</p>
+                        <p><b>Age:</b> 21 anos</p>
+                        <p><b>Email:</b> Aishra@hotmail.com</p>
+                        <a href=""> View</a>
+
+                </div>
+            </div>
+            <div>
+                <div class="desc">
+                    <h3>Andressa</h3>
+                 
+                        <p><b>Name:</b> Aishra Valentini</p>
+                        <p><b>Age:</b> 21 anos</p>
+                        <p><b>Email:</b> Aishra@hotmail.com</p>
+                        <a href=""> View</a>
+
+                </div>
+            </div><div>
+                <div class="desc">
+                    <h3>Andressa</h3>
+                 
+                        <p><b>Name:</b> Aishra Valentini</p>
+                        <p><b>Age:</b> 21 anos</p>
+                        <p><b>Email:</b> Aishra@hotmail.com</p>
+                        <a href=""> View</a>
+
+                </div>
+            </div><div>
+                <div class="desc">
+                    <h3>Andressa</h3>
+                 
+                        <p><b>Name:</b> Aishra Valentini</p>
+                        <p><b>Age:</b> 21 anos</p>
+                        <p><b>Email:</b> Aishra@hotmail.com</p>
+                        <a href=""> View</a>
+
+                </div>
+            </div>
+
+
+
+
+
+            <!-- termina os exemplos -->
+        </div>
+    </main>    
+ 
+    <?php
+
+include "../templates/rodape.html";
+
+?>
+   <script src="../../js/jquery.js"></script>
+  
+  <script src="../../js/main.js"></script>
+  <script>
+        $("#campos").hide();
+        $("#pesquisar").click(function(){
+            $("#campos").fadeToggle();
+            $("#pesquisar").hide();
+        });
+  </script>
---
+++ view/pages/Inglês/Tela Inicial/ingles - tela inicial.php
@@ -0,0 +1,144 @@
+<?php
+
+    include "../templates/menu.html";
+
+?>
+<div class="ConteudoInicial">
+        <div class="telainicial">
+            <div class="parte1">
+                <div class="titulo">
+                    <h1 style="color:black">NovAges</h1>
+                </div>
+                <div class="carrossel">
+                    <!-- <div class="conteudo"> -->
+                        
+                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                              <div class="carousel-item active">
+                                <a href="">
+                                    <img src="../../css/img/fundos/novAges.png" alt="">
+                                </a>
+                              </div>
+                              <div class="carousel-item">
+                                <img src="../../css/img/fundos/novAges.png" alt="">
+
+                              </div>
+                              <div class="carousel-item">
+                                <img src="../../css/img/fundos/novAges.png" alt="">
+
+                              </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="sr-only" style="background-color: black;">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    <!-- </div> -->
+                </div>
+            </div>
+            <div class="parte2">
+                <div>
+                    <div class="titulo">
+                        <h1 style="color:black">Special Guests</h1>
+                    </div>
+                    <div class="convidados">
+                        <section class="uruguay">
+                            <div class="pais">
+                                <h2><b>Brazil and Uruguay</b></h2>
+                            </div>
+                            <div class="pessoas">
+                                <div id="pessoa1">
+                                    <div class="desc" >
+                                        <h3>Andressa</h3>
+                                     
+                                            <p><b>Name:</b> Aishra Valentini</p>
+                                            <p><b>Age:</b> 21 years</p>
+                                            <p><b>Email:</b> Aishra@hotmail.com</p>
+                                            <a href=""> View</a>
+                                    </div>
+                                </div>
+                                <div id="pessoa2">
+                                    <div class="desc">
+                                        <h3>Andressa</h3>
+                                     
+                                            <p><b>Name:</b> Aishra Valentini</p>
+                                            <p><b>Age:</b> 21 years</p>
+                                            <p><b>Email:</b> Aishra@hotmail.com</p>
+                                            <a href=""> View</a>
+                                    </div>
+                                </div>
+                                <div id="pessoa3">
+                                    <div class="desc">
+                                        <h3>Andressa</h3>
+                                     
+                                            <p><b>Name:</b> Aishra Valentini</p>
+                                            <p><b>Age:</b> 21 years</p>
+                                            <p><b>Email:</b> Aishra@hotmail.com</p>
+                                            <a href=""> View</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="brasil">
+                            <div class="pais">
+                                <h2><b>Outros Paises</b></h2>
+                            </div>
+                            <div class="pessoas">
+                                <div id="pessoa4">
+                                    <div class="desc">
+                                            <h3>Andressa</h3>
+                                         
+                                                <p><b>Name:</b> Aishra Valentini</p>
+                                                <p><b>Age:</b> 21 years</p>
+                                                <p><b>Email:</b> Aishra@hotmail.com</p>
+                                                <a href="../convidadosespeciais/especifico.php"> View</a>
+                                    </div>
+                                </div>
+                                        <div id="pessoa5">
+                                            <div class="desc">
+                                                <h3>Andressa</h3>
+                                             
+                                                    <p><b>Name:</b> Aishra Valentini</p>
+                                                    <p><b>Age:</b> 21 years</p>
+                                                    <p><b>Email:</b> Aishra@hotmail.com</p>
+                                                    <a href=""> View</a>
+                                                    
+                                                </div>
+                                            </div>
+                                            <div id="pessoa6">
+                                                <div class="desc">
+                                                    <h3>Andressa</h3>
+                                                 
+                                                        <p><b>Name:</b> Aishra Valentini</p>
+                                                        <p><b>Age:</b> 21 years</p>
+                                                        <p><b>Email:</b> Aishra@hotmail.com</p>
+                                                        <a href=""> View</a>
+
+                                                </div>
+                                            </div>
+                            </div>
+                        </section>
+                        <center>
+                            <a href="" class="red-btn">See More</a>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <?php
+
+    include "../templates/rodape.html";
+
+?>
